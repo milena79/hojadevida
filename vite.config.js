7a69e4c5543319ee9057833213ecaa60acf3315f
milena79 <sandra.cruz@unad.edu.co>
2025-06-13 08:22:52 -0500

    ma ya

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: '/HojaDeVida-main',  // Asegúrate de que esta ruta sea la correcta para tu despliegue
-  build: {
-    outDir: 'dist',  // Esto debería coincidir con la carpeta en Netlify
-    target: 'esnext', // Especifica el objetivo de construcción para los navegadores modernos
-    modulePreload: true,
+  plugins: [react()],
+  server: {
+    port: 3000, // Puerto fijo
+    open: true,  // Abre el navegador automáticamente
   },
 });
