f2db985e331b9eecf692bc04c3ac6bf8c1fd665f
milena79 <sandra.cruz@unad.edu.co>
2025-06-13 08:06:41 -0500

    cambios en el archivo vite 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '//HojaDeVida-main',  // Asegúrate de que esta ruta sea la correcta para tu despliegue
+  base: '/HojaDeVida-main',  // Asegúrate de que esta ruta sea la correcta para tu despliegue
   build: {
     outDir: 'dist',  // Esto debería coincidir con la carpeta en Netlify
     target: 'esnext', // Especifica el objetivo de construcción para los navegadores modernos
